8ba35d4dc8acc90ede4928613077a90ca2d3eda9
Shweta2105 <sghadi21@gmail.com>
2021-06-20 23:35:45 +0530

    TC1.1 checked

--- test/java/junit_prob/practiceProb/MoodTest.java
+++ test/java/junit_prob/practiceProb/MoodTest.java
@@ -9,7 +9,7 @@ public class MoodTest {
 	@Test
 	public void getmoodByPassingMsg() {
 		MoodAnaylizer mood = new MoodAnaylizer();
-		Assert.assertEquals("SAD", mood.getMood("I am sad"));
+		Assert.assertEquals("SAD", mood.getMood("I am in sad mood"));
 		
 		
 	}
